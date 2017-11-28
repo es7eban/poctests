d96cf34292c5a6f65610ac9573d4310f904aa8a1
Esteban Arredondo <es_7eban@hotmail.com>
2017-11-28 09:03:47 +0100

    start importing

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
