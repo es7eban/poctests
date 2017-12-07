b4e200300660119b7ea7e53904521969a3894b3f
Esteban Arredondo <earredondo@Estebans-MBP.6min.lan>
2017-12-07 11:03:46 +0100

    fix format senteces

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { RouterModule, Routes } from "@angular/router"
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
-import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import { FormPostComponent } from './form-post/form-post.component';
 import { ListPostComponent } from './list-post/list-post.component';
-import {PostsService} from "./services/posts.service";
+import {PostsService} from './services/posts.service';
 
 const routes: Routes = [
-  {path:'edit/:id',component: FormPostComponent},
-  {path:'',component: ListPostComponent}
+  {path: 'edit/:id', component: FormPostComponent},
+  {path: '', component: ListPostComponent}
 ];
 
 @NgModule({
--- angular/src/app/services/posts.service.ts
+++ angular/src/app/services/posts.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 
-import { Post } from "../classes/post";
+import { Post } from '../classes/post';
 import { Http, Response, Headers } from '@angular/http';
-import 'rxjs/Rx';
-import {Observable} from 'rxjs/Rx';
+import {Observable} from 'rxjs/Observable';
 
 
 
@@ -13,25 +12,25 @@ export class PostsService {
 
   constructor(private http: Http) { }
 
-  getPosts(): Observable<Post[]>{
+  getPosts(): Observable<Post[]> {
     return this.http.get(this.apiUrl)
       .map((response: Response) => response.json());
   }
 
-  getPostById(id:string): Observable<Post>{
-    return this.http.get(this.apiUrl+'/edit/'+id)
+  getPostById(id: string): Observable<Post> {
+    return this.http.get(this.apiUrl + '/edit/' + id)
       .map((response: Response) => response.json());
   }
 
-  updatePost(id:string, body:string):Observable<any> {
-    let headers = new Headers();
+  updatePost(id: string, body: string): Observable<any> {
+    const headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    return this.http.post(this.apiUrl+'/update/'+id,body,{headers:headers})
+    return this.http.post(this.apiUrl + '/update/' + id, body, {headers: headers})
       .map((response: Response) => response.json());
   }
 
-  deletePost(id: string):Observable<any> {
-    return this.http.get(this.apiUrl+'/delete/'+id)
+  deletePost(id: string): Observable<any> {
+    return this.http.get(this.apiUrl + '/delete/' + id)
       .map((response: Response) => response.json());
   }
 }
